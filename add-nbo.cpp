aae694447fb72de33a2aae6fce408fafe040ec9a
jiwon-code <bani001019@gmail.com>
2021-07-21 09:02:27 -0700

    Add add-nbo

---
+++ add-nbo.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+
+int main(int argc, char *argv[]){
+
+    FILE *fd; 
+    
+    uint32_t a, b; 
+    
+    fd = fopen(argv[1], "r");
+    fread(&a, sizeof(a), 1, fd);
+    fd = fopen(argv[2], "r");
+    fread(&b, sizeof(b), 1, fd);
+
+    a = ntohl(a);
+    b = ntohl(b);
+    
+    printf("%d(0x%x)+%d(0x%x)=%d(0x%x)\n", a, a, b, b, a+b, a+b);
+    
+    return 0;
+}
